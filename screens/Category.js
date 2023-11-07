bbe94c96b7b2e757d2a3ef656c9f521452729983
백지환 <jihwan3118@naver.com>
2023-11-07 20:00:35 +0900

    UPDATE
    1. 차트 업데이트

--- screens/Category.js
+++ screens/Category.js
@@ -7,9 +7,9 @@ import {
     StyleSheet,
     Dimensions,
 } from 'react-native'
-import {
-    PieChart,
-} from "react-native-chart-kit";
+// import {
+//     PieChart,
+// } from "react-native-chart-kit";
 import Icon from 'react-native-vector-icons/Ionicons'
 import React, { useState, useEffect } from 'react'
 import { useIsFocused } from '@react-navigation/native';
@@ -22,6 +22,14 @@ import {
     loadStartUps, 
 } from '../DB/LoadDB'
 
+//그래프 import
+import { PieChart } from "react-native-gifted-charts"; 
+
+// import {VictoryPie} from 'victory-native';
+
+// const graphicColor = ['#388087', '#6fb3b8', '#badfe7']; // Colors
+// const wantedGraphicData = [{ y: 10 }, { y: 50 }, { y: 40 }]; // Data that we want to display
+// const defaultGraphicData = [{ y: 0 }, { y: 0 }, { y: 100 }]; // Data used to make the animate prop work
 
 
 
@@ -142,8 +150,86 @@ const Category = (props) => {
     }, [isFocused]);
 
 
+    // const [graphicData, setGraphicData] = useState(defaultGraphicData);
 
+    // useEffect(() => {
+    //     setGraphicData(wantedGraphicData); // Setting the data that we want to display
+    // }, []);
 
+    const pieData = [
+        {
+            value: 47,
+            color: '#009FFF',
+            gradientCenterColor: '#006DFF',
+            focused: true,
+        },
+        {value: 40, color: '#93FCF8', gradientCenterColor: '#3BE9DE'},
+        {value: 16, color: '#BDB2FA', gradientCenterColor: '#8F80F3'},
+        {value: 3, color: '#FFA5BA', gradientCenterColor: '#FF7F97'},
+    ];
+
+    const renderDot = color => {
+        return (
+            <View
+                style={{
+                    height: 10,
+                    width: 10,
+                    borderRadius: 5,
+                    backgroundColor: color,
+                    marginRight: 10,
+                }}
+            />
+        );
+    };
+
+    const renderLegendComponent = () => {
+          return (
+            <>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    marginBottom: 10,
+                    marginLeft: 50,
+                }}
+            >
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        width: 120,
+                        marginRight: 20,
+                    }}>
+                    {renderDot('#006DFF')}
+                    <Text style={{color: 'black'}}>Excellent: 47%</Text>
+                </View>
+                <View
+                    style={{flexDirection: 'row', alignItems: 'center', width: 120}}>
+                    {renderDot('#8F80F3')}
+                    <Text style={{color: 'black'}}>Okay: 16%</Text>
+                </View>
+            </View>
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        width: 120,
+                        marginRight: 20,
+                        marginLeft: 50,
+                    }}>
+                    {renderDot('#3BE9DE')}
+                    <Text style={{color: 'black'}}>Good: 40%</Text>
+                </View>
+                <View
+                    style={{flexDirection: 'row', alignItems: 'center', width: 120}}>
+                    {renderDot('#FF7F97')}
+                    <Text style={{color: 'black'}}>Poor: 3%</Text>
+                </View>
+              </View>
+            </>
+          );
+        };
 
     const data = [
         {
@@ -437,7 +523,44 @@ const Category = (props) => {
                             </Text>
                         </View>
                     </View>
-                    <View style={styles.trendImageView}>
+                    <View>
+                    {/* <VictoryPie
+                        animate={{ easing: 'exp' }}
+                        data={graphicData}
+                        width={250}
+                        height={250}
+                        colorScale={graphicColor}
+                        innerRadius={50}
+                    /> */}
+                    <View>
+                        <View style={{padding: 20, alignItems: 'center'}}>
+                            <PieChart
+                                data={pieData}
+                                donut
+                                showGradient
+                                sectionAutoFocus
+                                radius={90}
+                                innerRadius={60}
+                                focusOnPress
+                          
+                                centerLabelComponent={() => {
+                                    return (
+                                        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                                            <Text
+                                                style={{fontSize: 22, color: 'black', fontWeight: 'bold'}}>
+                                                47%
+                                            </Text>
+                                            <Text style={{fontSize: 14, color: 'black', fontWeight: 'bold'}}>Excellent</Text>
+                                        </View>
+                                    );
+                                }}
+                            />
+                        </View>
+                      {renderLegendComponent()}
+                    </View>
+                    
+                    </View>
+                    {/* <View style={styles.trendImageView}>
                         <PieChart
                             data={data}
                             width={Dimensions.get("window").width}
@@ -446,7 +569,7 @@ const Category = (props) => {
                             accessor={"population"}
                             backgroundColor={"transparent"}
                         />
-                    </View>
+                    </View> */}
                 </View>
             </View>
         </ScrollView>
