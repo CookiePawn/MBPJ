be1799553c0bde934e20f86ff3b81400dda3837b
안준철 <cookiepawn@naver.com>
2023-11-20 12:22:41 +0900

    UPDATE
    1. 차트 데이터 로드 성공

--- components/Category/CustomChart.js
+++ components/Category/CustomChart.js
@@ -1,64 +1,98 @@
-import { useEffect, useState } from 'react';
-import {
-    View,
-    Text,
-} from 'react-native'
-
-
-
-//그래프 import
-import { PieChart } from "react-native-gifted-charts";
-
-
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import { PieChart } from 'react-native-gifted-charts';
 import { loadConnect } from '../../DB/LoadDB';
 
-
-
-
-
-
 const CustomChart = () => {
-
-    const [connect, setConnect] = useState([])
+    const [connect, setConnect] = useState([]);
+    const [perField, setPerField] = useState({});
+    const [suField, setSuField] = useState({});
 
     useEffect(() => {
         const fetchConnect = async () => {
-            const connects = await loadConnect()
-            setConnect(connects)
-        }
-        fetchConnect()
-    }, [])
+            const connects = await loadConnect();
+            setConnect(connects);
+        };
+        fetchConnect();
+    }, []);
 
-    //차트 데이터
-    const pieData = [
-        {
-            value: 47,
-            color: '#009FFF',
-            gradientCenterColor: '#006DFF',
-            focused: true,
-        },
-        { value: 40, color: '#93FCF8', gradientCenterColor: '#3BE9DE' },
-        { value: 16, color: '#BDB2FA', gradientCenterColor: '#8F80F3' },
-        { value: 3, color: '#FFA5BA', gradientCenterColor: '#FF7F97' },
-    ];
+    useEffect(() => {
+        const perFieldCounts = calculatePerFieldCounts();
+        const suFieldCounts = calculateSuFieldCounts();
 
+        const perFieldMax = findMaxValue(convertToPieData(perFieldCounts));
+        const suFieldMax = findMaxValue(convertToPieData(suFieldCounts));
 
-    const renderDot = color => {
-        return (
-            <View
-                style={{
-                    height: 10,
-                    width: 10,
-                    borderRadius: 5,
-                    backgroundColor: color,
-                    marginRight: 10,
-                }}
-            />
-        );
+        setPerField({ maxKey: perFieldMax.maxKey, maxValue: perFieldMax.maxValue.toFixed(2) });
+        setSuField({ maxKey: suFieldMax.maxKey, maxValue: suFieldMax.maxValue.toFixed(2) });
+    }, [connect]);
+
+    const calculatePerFieldCounts = () => {
+        const perFieldCounts = {};
+
+        connect.forEach((data) => {
+            const { perField } = data;
+
+            if (perFieldCounts[perField]) {
+                perFieldCounts[perField]++;
+            } else {
+                perFieldCounts[perField] = 1;
+            }
+        });
+
+        return perFieldCounts;
     };
 
+    const calculateSuFieldCounts = () => {
+        const suFieldCounts = {};
 
-    const renderLegendComponent = () => {
+        connect.forEach((data) => {
+            const { suField } = data;
+
+            if (suFieldCounts[suField]) {
+                suFieldCounts[suField]++;
+            } else {
+                suFieldCounts[suField] = 1;
+            }
+        });
+
+        return suFieldCounts;
+    };
+
+    const convertToPieData = (fieldCounts) => {
+        const pieData = Object.keys(fieldCounts).map((key, index) => {
+            const value = fieldCounts[key];
+            const percentage = (value / connect.length) * 100;
+
+            return {
+                value: percentage,
+                color: getColor(index),
+                label: key,
+                gradientCenterColor: '#FF7F97',
+                focused: true,
+                percentageValue: `${percentage.toFixed(2)}%`,
+            };
+        });
+
+        return pieData;
+    };
+
+    const getColor = (index) => {
+        const colors = ['#009FFF', '#93FCF8', '#BDB2FA', '#FFA5BA'];
+        return colors[index % colors.length];
+    };
+
+    const renderLegendComponent = (fieldCounts, fieldName) => {
+        const pieData = convertToPieData(fieldCounts).slice(0, 4);
+
+        let maxKey, maxValue;
+        if (fieldName === 'perField') {
+            maxKey = perField.maxKey;
+            maxValue = perField.maxValue;
+        } else if (fieldName === 'suField') {
+            maxKey = suField.maxKey;
+            maxValue = suField.maxValue;
+        }
         return (
             <>
                 <View
@@ -105,75 +139,100 @@ const CustomChart = () => {
                 </View>
             </>
         );
+
+
+
+
+
+
+        return (
+            <>
+                <View style={{ flexDirection: 'row', justifyContent: 'center', marginBottom: 10 }}>
+                    {pieData.map((data, index) => (
+                        <View key={index} style={{ flexDirection: 'row', alignItems: 'center', width: 120 }}>
+                            {renderDot(data.color)}
+                            <Text style={{ color: 'black' }}>{`${data.label}: ${data.percentageValue}`}</Text>
+                        </View>
+                    ))}
+                </View>
+            </>
+        );
     };
 
+    const findMaxValue = (fieldCounts) => {
+        let maxValue = 0;
+        let maxKey = null;
+    
+        Object.keys(fieldCounts).forEach((key) => {
+            const value = fieldCounts[key].value;
+            if (value > maxValue) {
+                maxValue = value;
+                maxKey = fieldCounts[key].label; // 수정된 부분
+            }
+        });
+    
+        return { maxValue, maxKey };
+    };
 
-
+    const renderDot = (color) => {
+        return (
+            <View
+                style={{
+                    height: 10,
+                    width: 10,
+                    borderRadius: 5,
+                    backgroundColor: color,
+                    marginRight: 10,
+                }}
+            />
+        );
+    };
 
     return (
-        <View style={{marginBottom: 50,}}>
+        <View style={{ marginBottom: 50 }}>
             <View>
-                <View style={{ padding: 20, alignItems: 'center', }}>
+                <View style={{ padding: 20, alignItems: 'center' }}>
                     <PieChart
-                        data={pieData}
+                        data={convertToPieData(calculatePerFieldCounts())}
                         donut
                         showGradient
                         sectionAutoFocus
                         radius={90}
                         innerRadius={60}
                         focusOnPress
-
-                        centerLabelComponent={() => {
-                            return (
-                                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                                    <Text
-                                        style={{ fontSize: 22, color: 'black', fontWeight: 'bold' }}>
-                                        47%
-                                    </Text>
-                                    <Text style={{ fontSize: 14, color: 'black', fontWeight: 'bold' }}>Excellent</Text>
-                                </View>
-                            );
-                        }}
+                        centerLabelComponent={() => (
+                            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                <Text style={{ fontSize: 22, color: 'black', fontWeight: 'bold' }}>{perField.maxValue}%</Text>
+                                <Text style={{ fontSize: 14, color: 'black', fontWeight: 'bold' }}>{perField.maxKey}</Text>
+                            </View>
+                        )}
                     />
                 </View>
-                {renderLegendComponent()}
+                {renderLegendComponent(calculatePerFieldCounts(), 'perField')}
+            </View>
+
+            <View>
+                <View style={{ padding: 20, alignItems: 'center' }}>
+                    <PieChart
+                        data={convertToPieData(calculateSuFieldCounts())}
+                        donut
+                        showGradient
+                        sectionAutoFocus
+                        radius={90}
+                        innerRadius={60}
+                        focusOnPress
+                        centerLabelComponent={() => (
+                            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                <Text style={{ fontSize: 22, color: 'black', fontWeight: 'bold' }}>{suField.maxValue}%</Text>
+                                <Text style={{ fontSize: 14, color: 'black', fontWeight: 'bold' }}>{suField.maxKey}</Text>
+                            </View>
+                        )}
+                    />
+                </View>
+                {renderLegendComponent(calculateSuFieldCounts(), 'suField')}
             </View>
         </View>
-    )
+    );
+};
 
-}
-
-
-
-export default CustomChart
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default CustomChart;
